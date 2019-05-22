b575185ad34071c9e29a85524573cde9dc8d1799
Ondrej Chrastina <chrastina.ondra@gmail.com>
2019-05-22 19:33:18 +0200

    initial version of the custom element

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# gatsby-preview-custom-element
-Custom element allows to subscribe for Gatsby preview functionality
+# Kentico Cloud Gatsby Preview Custom Element
+
+Gatsby preview custom element is watching content changes.
+When the change is performed it sends the notification to the GatsbyJs and notify editor that the preview environment is ready to be previewed.
+
+![Gatsby preview custom element](gatsby-preview-custom-element.gif)
+
+## Usage
+
+If you want to use the Gatsby preview custom element in your project in Kentico Cloud, follow these steps:
+
+* In Kentico Cloud open Content types tab
+* Open / create a content model to which you want to add the Gatsby preview custom element
+* Add **Custom element** content element
+* Open configuration of the content element
+* Use following URL as Hosted code URL (HTTPS): https://Simply007.github.io/gatsby-preview-custom-element/dist/index.html
+* Provide the following JSON parameters for the custom element to connect it to the appropriate elements
+
+```json
+{
+    "previewApiKey": "<YOUR PREVIEW API KEY>",
+    "previewUrlPattern": "<URL WITH PREVIEW PATTERN MACROS>",
+    "urlSlugElement": "url_pattern",
+    "gatsbyWebSocketUrl": "wss://echo.websocket.org"
+}
+```
+## Installation
+
+Toast UI editor is in repository: https://github.com/Simply007/gatsby-preview-custom-element.
+
+If you want to adjust the implementation, first download [Kentico Cloud Custom Elements Devkit](https://github.com/kentico/custom-element-devkit). This repository should be positioned within `/client/custom-elements` folder. For further instructions on devkit implementation, please refer to [Custom Element Devkit README](https://github.com/Kentico/custom-element-devkit/blob/master/readme.md).
+
+### Get started
+
+You could use following commands to set up the development environment
+
+Prerequisites:
+
+* Node.js
+* git
+
+```plain
+git clone https://github.com/Kentico/custom-element-devkit.git
+cd custom-element-devkit
+git clone https://github.com/Simply007/gatsby-preview-custom-element.git ./client/custom-elements/gatsby-preview-custom-element
+npm install --save tui-editor
+npm start -- -hw
+```
+
+Browse: https://localhost:3000/custom-elements/gatsby-preview-custom-element
---
+++ gatsby-preview.js
@@ -0,0 +1,225 @@
+import './gatsby-preview.css';
+
+var lastModified = null;
+var waitTimeout = null;
+var urlSlugElement = null;
+var config = null;
+var language = null;
+var codename = null;
+var urlSlug = null;
+var projectId = null;
+var urlSlugRegex = /{urlslug}/ig;
+var langRegex = /{lang}/ig;
+var gatsbyWebSocket = null;
+
+
+
+function sendRequestToGatsby() {
+  console.log('Sending request to gatsby!');
+
+  // notifying web socket
+
+  gatsbyWebSocket.send(`Changes on item ${codename} performed!`);
+}
+
+function updateSize() {
+  // Update the custom element height in the Kentico UI.
+  const height = Math.ceil(document.querySelector("html").offsetHeight);
+  CustomElement.setHeight(height);
+}
+
+function hideAll() {
+  document.querySelector('.loader').style.display = 'none';
+  document.querySelector('.pending').style.display = 'none';
+  document.querySelector('.ready').style.display = 'none';
+  document.querySelector('.na').style.display = 'none';
+}
+
+function showNA() {
+  hideAll();
+  document.querySelector('.na').style.display = '';
+  updateSize();
+}
+
+function showPending(pendingText) {
+  document.querySelector('.circle-loader').classList.remove('load-complete')
+  document.querySelector('.ready').style.display = 'none';
+  document.querySelector('.pending h3').textContent = pendingText;
+  document.querySelector('.pending').style.display = '';
+  document.querySelector('.loader').style.display = '';
+  document.querySelector('.checkmark').style.display = 'none';
+  updateSize();
+}
+
+function getPreviewUrl() {
+  if ((urlSlugElement && !urlSlug) || !projectId) {
+    return null;
+  }
+  return config.previewUrlPattern.replace(urlSlugRegex, urlSlug).replace(langRegex, language);
+}
+
+function showReady() {
+  document.querySelector('.circle-loader').classList.add('load-complete');
+  document.querySelector('.ready').style.display = '';
+  document.querySelector('.loader').style.display = '';
+  document.querySelector('.pending').style.display = 'none';
+  document.querySelector('.checkmark').style.display = '';
+
+  var previewUrl = getPreviewUrl();
+  if (previewUrl) {
+
+    document.querySelector('.preview').setAttribute('href', previewUrl)
+    document.querySelector('.preview').style.display = '';
+  }
+  else {
+    document.querySelector('.preview').style.display = 'none';
+  }
+  updateSize();
+}
+
+
+function getItemUrl() {
+  if (!codename || !projectId) {
+    return null;
+  }
+  return 'https://preview-deliver.kenticocloud.com/' + projectId + '/items/' + codename;
+}
+
+function scheduleWaitForPreview() {
+  if (!waitTimeout) {
+    showPending("Checking preview availability ...");
+    waitTimeout = setTimeout(waitForPreview, 1000);
+  }
+}
+
+function fetchItem() {
+  var itemUrl = getItemUrl();
+  console.log(`calling for item: ${itemUrl}`);
+  return fetch(itemUrl, {
+    headers: new Headers({
+      'Authorization': `Bearer ${config.previewApiKey}`
+    })
+  });
+}
+
+function waitForPreview() {
+  waitTimeout = null;
+
+  if (lastModified) {
+    fetchItem()
+      .then((response) => response.json())
+      .then(({ item }) => {
+        console.log(`defined lastModified: ${lastModified.toISOString()}`);
+        console.log(`received item last modified: ${new Date(item.system.last_modified).toISOString()}`);
+        if (item && item.system && !lastModified
+          || (new Date(item.system.last_modified).getTime() >= lastModified.getTime())) {
+          lastModified = new Date(item.system.last_modified);
+          showPending("Sending notification to Gatsby...");
+          sendRequestToGatsby();
+        }
+        else {
+          scheduleWaitForPreview();
+        }
+      })
+      .catch(_ => {
+        console.error(err)
+        scheduleWaitForPreview();
+      });
+  } else { // initial load
+    fetchItem()
+      .then((response) => response.json())
+      .then(({ item }) => {
+        lastModified = new Date(item.system.last_modified);
+        showReady();
+      })
+      .catch(err => {
+        console.error(err)
+        scheduleWaitForPreview();
+      });
+
+  }
+}
+
+function load(updateUrlSlug) {
+  if (updateUrlSlug) {
+    CustomElement.getElementValue(urlSlugElement, (value) => {
+      urlSlug = value;
+      scheduleWaitForPreview();
+    });
+  }
+  else {
+    scheduleWaitForPreview();
+  }
+}
+
+function changed(changedElementCodenames) {
+  console.log(`changes: ${changedElementCodenames}`)
+  var updateUrlSlug = urlSlugElement && (changedElementCodenames.indexOf(urlSlugElement) >= 0);
+  load(updateUrlSlug);
+}
+
+function configValid() {
+  // TODO extend by the configuation contract
+  if (!config.previewApiKey) {
+    console.error('Missing preview API key.')
+    return false;
+  }
+  return true;
+}
+
+function initCustomElement() {
+  try {
+    CustomElement.init((element, context) => {
+      config = element.config || {};
+      if (!configValid()) {
+        throw Error('Configuration is not valid.')
+      }
+
+      gatsbyWebSocket = new WebSocket(config.gatsbyWebSocketUrl);
+
+      gatsbyWebSocket.onopen = function (evt) {
+        console.log("Connection to gatsby opened...");
+
+        projectId = context.projectId;
+        codename = context.item.codename;
+        language = context.variant.codename;
+        urlSlugElement = config.urlSlugElement;
+        if (codename) { // because custom element could be placed to content component - unsupported
+          load(!!urlSlugElement);
+        }
+        else {
+          showNA();
+        }
+
+        window.addEventListener('resize', updateSize);
+        CustomElement.observeElementChanges([], changed);
+      };
+      gatsbyWebSocket.onmessage = function (evt) {
+        console.log("Received Message: " + evt.data);
+        // TODO error handling
+        showReady();
+      };
+    });
+  }
+  catch (err) {
+    // Initialization with Kentico Custom element API failed (page displayed outside of the Kentico UI)
+    console.error(err);
+  }
+}
+
+hideAll();
+showPending("Checking preview availability ...");
+
+gatsbyWebSocket = new WebSocket("wss://echo.websocket.org");
+
+gatsbyWebSocket.onopen = function (evt) {
+  console.log("Connection to gatsby opened...");
+};
+
+gatsbyWebSocket.onmessage = function (evt) {
+  console.log("Received Message: " + evt.data);
+  // TODO error handling
+  showReady();
+};
+
+initCustomElement();
